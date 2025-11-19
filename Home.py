1d4b0e48cab3f891dd526d734aacaf60e5c20b95
Kanrawee2334 <kanrawee214@gmail.com>
2025-11-19 15:37:43 +0700

    N

--- Home.py
+++ Home.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 st.header("Karawee")
 st.image("./img/kanrawee.jpg")
 
+st.header("โปรเจคการจำแนกข้อมูลดอกไม้")
 #st.image("./img/kairung.jpg")
 col1, col2, col3 = st.columns(3)
 
